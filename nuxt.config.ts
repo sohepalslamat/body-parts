c0701d937d2387e4c37c622b53c4c57c56441684
Sohaib Alslamt <sohep.alslamat91@gmail.com>
2023-04-16 07:47:15 +0300

    remove base url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,7 @@ export default defineNuxtConfig({
       payloadExtraction: false
     },
     app: {
-      baseURL: '/body-parts/', // baseURL: '/<repository>/'
+      // baseURL: '/body-parts/', // baseURL: '/<repository>/'
       // buildAssetsDir: '/dist/_nuxt'
     },
 
